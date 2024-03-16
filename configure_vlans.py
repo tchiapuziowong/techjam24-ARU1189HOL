f52a1dda1eefac59501ef56ad58a82fb1a9a6366
Your Name <you@example.com>
2024-03-15 17:38:53 -0700

    init

--- configure_vlans.py
+++ configure_vlans.py
@@ -23,43 +23,43 @@ version = "10.09"
 vlan_list = [100]
 
 for _, switch in inventory.items():
-    if "role" in switch.keys() and switch["role"] == "access":
-        print("Connecting to switch {0}".format(switch["hostname"]))
-        switch_ip = switch["mgmt_int_ip"]
-        sesh = Session(switch_ip, version)
-        sesh.open(switch["login_user"], switch["login_pw"], use_proxy=False)
+    if "role" not in switch.keys() or switch["role"] != "access":
+        continue
+    print("Connecting to switch {0}".format(switch["hostname"]))
+    switch_ip = switch["mgmt_int_ip"]
+    sesh = Session(switch_ip, version)
+    sesh.open(switch["login_user"], switch["login_pw"], use_proxy=False)
 
-        sw = Device(sesh)
-        sw.session = sesh
-
+    sw = Device(sesh)
+    sw.session = sesh
+    # Try block is used so that session closes even on error.
+    try:
         for vlan in vlan_list:
-            # Try block is used so that session closes even on error.
+
+            print("Configuring VLAN {0}...".format(vlan))
+
+            # Create VLAN obj
+            tmp_vlan = sw.vlan(vlan)
+
+            # apply() will create the VLAN or update it if existing
             try:
-                print("Configuring VLAN {0}...".format(vlan))
+                tmp_vlan.apply()
+            except Exception:
+                pass
 
-                # Create VLAN obj
-                tmp_vlan = sw.vlan(vlan)
-
-                # apply() will create the VLAN or update it if existing
-                try:
-                    tmp_vlan.apply()
-                except Exception:
-                    pass
-
-                if not tmp_vlan.materialized:
-                    print("ERROR: VLAN {0} configuration failure".format(vlan))
-                    quit(-1)
-                else:
-                    print(
-                        "VLAN{0} configured successfully on {1}".format(
-                            vlan, switch["hostname"]
-                        )
+            if not tmp_vlan.materialized:
+                print("ERROR: VLAN {0} configuration failure".format(vlan))
+                quit(-1)
+            else:
+                print(
+                    "VLAN{0} configured successfully on {1}".format(
+                        vlan, switch["hostname"]
                     )
-                # Example of configuring interface with VLAN
+                )
 
-            except Exception as error:
-                print("Ran into exception: {0}. Closing session.".format(error))
+    except Exception as error:
+        print("Ran into exception: {0}. Closing session.".format(error))
 
-            finally:
-                # At the end, the session MUST be closed
-                sesh.close()
+    finally:
+        # At the end, the session MUST be closed
+        sesh.close()
